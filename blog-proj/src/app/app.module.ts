acee0ec54fe4a8d59e7cc5f38d46093963c618fc
rafisgarrido <rafascgarrido@gmail.com>
2023-09-24 22:10:21 -0300

    atualizações em andamento

--- blog-proj/src/app/app.module.ts
+++ blog-proj/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { SmallCardComponent } from './components/small-card/small-card.component
 import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ContentComponent } from './pages/content/content.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { HomeComponent } from './pages/home/home.component';
     SmallCardComponent,
     MenuTitleComponent,
     MenuBarComponent,
-    HomeComponent
+    HomeComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
